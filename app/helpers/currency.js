8b05336fd3914d22dd3a4f9c15c8a634b64b1e14
fergus-g <fergus.gildea@utrack.com>
2025-02-23 16:39:31 +0000

    Inital Commit

---
+++ app/helpers/currency.js
@@ -0,0 +1,15 @@
+import Helper from '@ember/component/helper';
+
+export default class currency extends Helper {
+  compute(positional, named = {}) {
+    const [number] = positional;
+    const { sign = '€' } = named;
+    const euro = Math.floor(number);
+    let cents = Math.floor((number * 100) % 100);
+
+    if (cents.toString().length === 1) {
+      cents = '0' + cents;
+    }
+    return `${sign}${euro}.${cents}`;
+  }
+}
